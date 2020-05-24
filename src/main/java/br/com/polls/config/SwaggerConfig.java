a25af9d5e57ef2873d8fdacd996df63f60ea9223
SmythyCosta <smythy.costa@hotmail.com>
2020-05-24 20:56:39 -0300

    new class Swagger config

---
+++ src/main/java/br/com/polls/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package br.com.polls.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.ApiKeyVehicle;
+import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@Profile("dev")
+@EnableSwagger2
+public class SwaggerConfig {
+
+
+}
+
