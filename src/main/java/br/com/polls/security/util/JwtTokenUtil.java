651fd7c41174985bb0f9f43360189874c386fe16
SmythyCosta <smythy.costa@hotmail.com>
2020-05-24 22:03:47 -0300

    class JwtTokenUtil

--- src/main/java/br/com/polls/security/util/JwtTokenUtil.java
+++ src/main/java/br/com/polls/security/util/JwtTokenUtil.java
@@ -1,5 +1,67 @@
 package br.com.polls.security.util;
 
-public class JwtTokenUtil {
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+
+@Component
+public class JwtTokenUtil {
+	
+	static final String CLAIM_KEY_USERNAME = "sub";
+	static final String CLAIM_KEY_ROLE = "role";
+	static final String CLAIM_KEY_AUDIENCE = "audience";
+	static final String CLAIM_KEY_CREATED = "created";
+
+	@Value("${app.jwtSecret}")
+	private String secret;
+
+	@Value("${app.jwtExpirationInMs}")
+	private Long expiration;
+	
+	/**
+	 * Returns a new JWT token based on user data.
+	 * 
+	 * @param userDetails
+	 * @return String
+	 */
+	public String obterToken(UserDetails userDetails) {
+		Map<String, Object> claims = new HashMap<>();
+		claims.put(CLAIM_KEY_USERNAME, userDetails.getUsername());
+		userDetails.getAuthorities().forEach(authority -> claims.put(CLAIM_KEY_ROLE, authority.getAuthority()));
+		claims.put(CLAIM_KEY_CREATED, new Date());
+
+		return gerarToken(claims);
+	}
+	
+	/**
+	 * Generates a new JWT token containing the data (claims) provided.
+	 * 
+	 * @param claims
+	 * @return String
+	 */
+	private String gerarToken(Map<String, Object> claims) {
+		return Jwts.builder().setClaims(claims).setExpiration(generateExpirationDate())
+				.signWith(SignatureAlgorithm.HS512, secret).compact();
+	}
+	
+	/**
+	 * Returns the expiration date based on the current date.
+	 * 
+	 * @return Date
+	 */
+	private Date generateExpirationDate() {
+		return new Date(System.currentTimeMillis() + expiration * 1000);
+	}
+
+	
 }
