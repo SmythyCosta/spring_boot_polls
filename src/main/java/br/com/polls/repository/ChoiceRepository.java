d7969a07ffd59624b880fbf91f0e4af168075295
SmythyCosta <smythy.costa@hotmail.com>
2020-05-30 15:09:58 -0300

    repository

--- src/main/java/br/com/polls/repository/ChoiceRepository.java
+++ src/main/java/br/com/polls/repository/ChoiceRepository.java
@@ -2,15 +2,25 @@ package br.com.polls.repository;
 
 import java.util.Optional;
 
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
 
 import br.com.polls.model.Choice;
 
 
+@Transactional(readOnly = true)
+@NamedQueries({
+		@NamedQuery(name = "ChoiceRepository.findChoiceById", 
+				query = "SELECT ch FROM Choice ch WHERE ch.id = :choiceId") })
 @Repository
 public interface ChoiceRepository extends JpaRepository<Choice, Long> {
 
-    Optional<Choice> findById(Long choiceId);
+    Optional<Choice> findChoiceById(@Param("choiceId") Long choiceId);
 
 }
