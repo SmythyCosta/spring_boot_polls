8c4aeb33c2837c2f0ee465acea2bea3344c2b87d
SmythyCosta <smythy.costa@hotmail.com>
2020-06-04 00:03:51 -0300

    layer dto

--- src/main/java/br/com/polls/payload/UserSummary.java
+++ src/main/java/br/com/polls/payload/UserSummary.java
@@ -1,5 +1,10 @@
 package br.com.polls.payload;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserSummary {
 	
     private Long id;
@@ -12,28 +17,5 @@ public class UserSummary {
         this.name = name;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
 
